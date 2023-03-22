9bcd8c74fbd99aaeeb10e307aa6eb3f806cb3458
luis.perrotta <luis.perrotta@uap.edu.ar>
2023-03-21 22:19:21 -0300

    prueba final

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
 import React, { useEffect, useState } from "react";
-
 export const App = () => {
-  const GIPHY_API_KEY = "N79DhT5EQxbkZQFQkEG9QgVYcdYRBhFV"
+  const GIPHY_API_KEY = "dc6zaTOxFJmzC"
   const [catFact, setCatFact] = useState('')
   const [catGif, setCatGif] = useState('')
 
   function BotonEjecutar(props) {
     return (
-      <button onClick={props.handleClick}>
-        Ejecutar Función
+      <button onClick={props.handleClick} style={{ backgroundColor: 'blue', color: 'white', borderRadius: '5px', boxShadow: '2px 2px 2px grey' }}>
+        Click Aqui!
       </button>
     );
   }
 
+  function Navbar() {
+    return (
+      <nav style={{ backgroundColor: 'black', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80px', fontFamily: 'Courier New' }}>
+        <h1 style={{ fontSize: '36px' }}>TP Tec Emergentes</h1>
+      </nav>
+    );
+  }
+
   const callGiphyAPI = (string) => {
-    fetch(`https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`)
-    .then((Response) => Response.json())
-    .then((data) => setCatGif( data.data[0].url))
-    //.then((data) => console.log( data.data[0].url))
-    
+    // const word = "bear"; // aquí puedes reemplazar "cat" con una variable que contenga la cadena de búsqueda del usuario
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=XaGG5vM6Q5p8mUkhCV4QdbTtwfgEgHSD&q=${string}&limit=25&offset=0&rating=g&lang=en`, { mode: "cors" })
+      // fetch(`https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`, { mode: "cors" })
+
+      .then((Response) => Response.json())
+      .then((data) => setCatGif(data.data[0]?.images?.original?.url || null))
+      .catch((error) => {
+        console.error(error);
+        setCatGif('https://via.placeholder.com/500x500?text=Error+al+cargar+la+imagen');
+      });
   };
   const callAPI = () => {
     fetch('https://catfact.ninja/fact')
-    .then(Response => Response.json())
-    .then((data) => {
-      setCatFact(data.fact || 'hola michi michi');
-      callGiphyAPI(data?.fact?.split(" ").slice(0,3).join(' '))
-    });
+      .then(Response => Response.json())
+      .then((data) => {
+        setCatFact(data.fact || 'hola michi michi');
+        callGiphyAPI(data?.fact?.split(" ").slice(0, 3).join(' '))
+      });
   };
+
+
   useEffect(callAPI, []);
-  return(
+  console.log(catGif);
+  return (
     <>
-    <h1>{catFact}</h1>
-    <img alt="inspeccionar imagen, no renderiza por los cors" src={catGif} crossOrigin="true"/>
-    <BotonEjecutar handleClick={callAPI} />
+      <div style={{ backgroundImage: "url('fondo.jpg')", backgroundSize: 'cover', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', fontFamily: 'Courier New' }}>
+        <Navbar />
+        <h1 style={{ fontSize: '24px', color: 'white', backgroundColor: 'grey' }}>{catFact}</h1>
+        <img alt="imagen de gato aleatoria" src={catGif} crossOrigin="true"
+          style={{
+            width: '300px',
+            height: '300px',
+            borderRadius: '10%',
+            boxShadow: '2px 2px 2px grey'
+          }} />
+        <br />
+        <BotonEjecutar handleClick={callAPI} />
+      </div>
     </>
-  ); 
+  );
 };
 export default App;
+
